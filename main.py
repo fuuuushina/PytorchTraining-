d6190c814c14381ccc25b508e56fa1fb18064040
fuuuushina <emilien.joyeux87@gmail.com>
2025-10-27 10:35:54 +0100

    quick start

--- main.py
+++ main.py
@@ -51,10 +51,11 @@ class NeuralNetwork(nn.Module):
             nn.Linear(512, 10)
         )
 
-def forward(self, x):
-    x = self.flatten(x)
-    logits = self.linear_relu_stack(x)
-    return logits
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
 
 model = NeuralNetwork().to(device)
 print(model)
@@ -76,3 +77,29 @@ def train(dataloader, model, loss_fn, optimizer):
        loss.backward()
        optimizer.step()
        optimizer.zero_grad()
+
+       if batch % 100 == 0:
+           loss, current = loss.item(), (batch + 1) * len(X)
+           print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X,  y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+        test_loss /= num_batches
+        correct /= size
+        print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch {t + 1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
